396e3ea6fd0fa936b85a5cfcab9f9695c2dad9a7
Afsoon <afsoona@cs.cmu.edu>
2015-12-14 16:09:11 -0500

    pycparser is not as powerfull as I hoped so I changed the parser to libclang and tested it with large c files. this is the first draft using libclang

--- fault_localization/suspicious_block.py
+++ fault_localization/suspicious_block.py
@@ -1,46 +1,53 @@
 __author__ = 'Afsoon Afzal'
 
-from pycparser import c_parser, c_ast, parse_file
-from process_program import ProcessProgram
-from pycparser.c_ast import If
+from clang.cindex import *
 
 
 class FaultLocalization():
-    def __init__(self, program):
-        self.program = program
+    def __init__(self, filename):
+        Config.set_library_file('/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/libclang.dylib')
+        self.filename = filename
+        self.root = None
 
     def line_to_block(self, line_number):
-        parser = c_parser.CParser()
-        ast = parser.parse(self.program.processed_text, filename='<none>')
-        ast.show()
-        return self.traverse_tree(line_number, ast)
+        index = Index.create()
+        self.root = index.parse(self.filename)
+        return self.traverse_tree(line_number, self.root.cursor)
 
     def traverse_tree(self, line_number, ast):
-        block = (1, self.program.total_lines)
+        assert (isinstance(ast, Cursor))
+        block = (1, 10000000)
         current = ast
-        children = ast.children()
+        children = ast.get_children()
         cond = True
         while(cond):
-            print block
-            for temp, child in children:
-                print child.coord.line
-                if child.coord.line > line_number:
-                    temp_block = (current.coord.line, child.coord.line)
-                    if temp_block[1] - temp_block[0] < 6:
+            cond = False
+            for child in children:
+                cond = True
+                print block
+                #print str(child.spelling) + " " + str(child.location.file)
+                if str(child.location.file) != self.filename:
+                    continue
+                #print child.location.line
+                if child.location.line > line_number:
+                    temp_block = (current.location.line, child.location.line)
+                    if current.kind == CursorKind.IF_STMT:
                         cond = False
-                    else:
-                        block = temp_block
-                    if isinstance(current, If):
+                    elif temp_block[1] - temp_block[0] < 4:
                         cond = False
+                        if block[1] - block[0] > 6:
+                            block = (temp_block[1] - 6, temp_block[1])
+                        break
+                    block = temp_block
                     break
                 current = child
-            children = current.children()
+            children = current.get_children()
+
+
 
         return block
 
 
 if __name__ == "__main__":
-    pp = ProcessProgram('median.c')
-    print pp.processed_text
-    fl = FaultLocalization(pp)
-    print fl.line_to_block(19)
+    fl = FaultLocalization('src/joblist.c')
+    print fl.line_to_block(54)
