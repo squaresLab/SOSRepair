89a501fc340aab78b5bea51476ca6e13df52fcde
Afsoon <afsoona@cs.cmu.edu>
2016-06-12 16:24:51 -0400

    Fix issue of z3 encoding of negative numbers

--- utils/z3.py
+++ utils/z3.py
@@ -27,5 +27,5 @@ def run_z3(query):
 def twos_comp(val, bits):
     """compute the 2's compliment of int value val"""
     if (val & (1 << (bits - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
-        val = val - (1 << bits)        # compute negative value
+        val = -(val - (1 << bits))        # compute negative value
     return val                         # return positive value as is
