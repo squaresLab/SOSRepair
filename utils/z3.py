c35d3596ade322a523796671e37abc4ee7c8e2c5
Afsoon <afsoona@cs.cmu.edu>
2017-02-20 10:49:26 -0500

    Set timeout for Z3

--- utils/z3.py
+++ utils/z3.py
@@ -2,6 +2,8 @@ __author__ = 'Afsoon Afzal'
 
 import subprocess
 import os
+import threading
+from utils import timeout, kill_check
 from settings import Z3_COMMAND
 
 
@@ -10,23 +12,28 @@ def run_z3(query):
     values = []
     with open('z3_query.smt2', 'w') as f:
         f.write(query)
-        f.flush()
-        proc = subprocess.Popen(Z3_COMMAND + ' -smt2 z3_query.smt2', stdout=subprocess.PIPE, shell=True)
-        (out, err) = proc.communicate()
-        print "**** " + out
-        if err:
-            print "ERROR: z3"
-        for l in out.splitlines():
-            if l == 'sat':
-                ret = True
-            elif l == 'unsat':
-                ret = False
-            if l.startswith('(('):
-                l_temp = l[2:-2]
-                parts = l_temp.split(' ')
-                if len(parts) != 2:
-                    print "ERROR: Why?"
-                values.append((parts[0][2:], int(parts[1])))
+    proc = subprocess.Popen(Z3_COMMAND + ' -smt2 z3_query.smt2', shell=True, stdout=subprocess.PIPE, preexec_fn=os.setsid)
+    t = threading.Timer(30, timeout, [proc])
+    t.start()
+    (out, err) = proc.communicate()
+    t.cancel()
+    if kill_check.isSet() or proc.returncode:
+        kill_check.clear()
+        return False, []
+    print "**** " + out
+    if err:
+        print "ERROR: z3"
+    for l in out.splitlines():
+        if l == 'sat':
+            ret = True
+        elif l == 'unsat':
+            ret = False
+        if l.startswith('(('):
+            l_temp = l[2:-2]
+            parts = l_temp.split(' ')
+            if len(parts) != 2:
+                print "ERROR: Why?"
+            values.append((parts[0][2:], int(parts[1])))
     os.system('rm z3_query.smt2')
     return ret, values
 
